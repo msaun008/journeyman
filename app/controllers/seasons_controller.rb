d5e542cf98142bf6e2f083e5482f9339a3ecb257
Matthew Saunders <matthew.saunders12@gmail.com>
2017-06-13 22:12:54 -0400

    Add creational verbs to season implementation

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -1,15 +1,53 @@
 class SeasonsController < ApplicationController
 
+  before_action :find_season, only: [:show, :edit, :update, :destroy]
+
   def index
     @seasons = Season.all
   end
 
   def show
-    @season = Season.find(params[:id])
+  end
+
+  def new
+    @season = Season.new
+  end
+
+  def create
+    @season = Season.new(season_params)
+
+    if @season.save
+      redirect_to @season
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @season.update(season_params)
+      redirect_to @season
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    if @season.destroy
+      redirect_to seasons_path
+    else
+      redirect_to @season
+    end
   end
 
   private
     def season_params
       params.require(:season).permit(:name)
+    end
+
+    def find_season
+      @season = Season.find(params[:id])
     end
 end
