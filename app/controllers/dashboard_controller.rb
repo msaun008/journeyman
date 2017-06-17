975a0a774f6460c2f3151aaa53b77e7ef4c4d83e
Matthew Saunders <matthew.saunders12@gmail.com>
2017-06-17 15:49:40 -0400

    Create a basic show page for season

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,13 @@
 class DashboardController < ApplicationController
 
   def home
-    @seasons = Season.all
+    @seasons = [Season.first]
   end
+
+  private
+    def get_active_seasons
+      # TODO Refine this algorithm as seasons evolve during development
+      Season.joins(:events).group('season.id').to_a;
+    end
 
 end
