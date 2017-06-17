e048b351ebcaf8969b55df9eb35d665b0fb8a51a
Matthew Saunders <matthew.saunders12@gmail.com>
2017-06-17 14:22:34 -0400

    Add season display to dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class DashboardController < ApplicationController
 
   def home
-
+    @seasons = Season.all
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   root 'dashboard#home'
 
   resources :seasons
+  resources :events
 end
