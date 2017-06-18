8af09329a76dcc309aa851f2658b7eb70100a8e7
Matthew Saunders <matthew.saunders12@gmail.com>
2017-06-18 15:57:03 -0400

    Update db seeding and event tile styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,20 @@
 seasons = Season.create([
   { name: "Spring 2017" },
-  { name: "Summer 2017" },
-  { name: "Fall 2017" },
-  { name: "Winter 2017" },
+  # { name: "Summer 2017" },
+  # { name: "Fall 2017" },
+  # { name: "Winter 2017" },
 ])
 
 events = Event.create([
-  { start_date: DateTime.new(2017,2,3,4,5,6), duration: 1, location: "Melbourne", station: "KMLB", season: seasons[0] },
-  { start_date: DateTime.new(2017,2,3,4,5,6), duration: 1, location: "Miami", station: "KMIA", season: seasons[0] },
-  { start_date: DateTime.new(2017,2,3,4,5,6), duration: 1, location: "Tallahassee", station: "KTLH", season: seasons[0] },
+  { start_date: DateTime.new(2017,6,1,0,0,0), duration: 1, location: "Melbourne, FL", station: "KMLB", season: seasons[0] },
+  { start_date: DateTime.new(2017,7,1,0,0,0), duration: 1, location: "Miami, FL", station: "KMIA", season: seasons[0] },
+  { start_date: DateTime.new(2017,8,1,0,0,0), duration: 1, location: "Tallahassee, FL", station: "KTLH", season: seasons[0] },
+  { start_date: DateTime.new(2017,9,1,0,0,0), duration: 1, location: "Boston, MA", station: "KBOS", season: seasons[0] },
+])
+
+forecasts = Forecast.create([
+  { high_temp: 90, low_temp: 70, precipitation: 0.25, wind: 12, score: 27.9, season: seasons[0], event: events[0] },
+  { high_temp: 90, low_temp: 70, precipitation: 0.25, wind: 12, score: 0, season: seasons[0], event: events[1] },
+  { high_temp: 90, low_temp: 70, precipitation: 0.25, wind: 12, score: 0, season: seasons[0], event: events[2] },
+  { high_temp: 90, low_temp: 70, precipitation: 0.25, wind: 12, score: 0, season: seasons[0], event: events[3] },
 ])
