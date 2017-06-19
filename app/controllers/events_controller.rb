95eb3cf6e5fe3a73c6893429f8f3a4e672b455de
Matthew Saunders <matthew.saunders12@gmail.com>
2017-06-19 09:13:18 -0400

    Format and style event view to include the forecast

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,8 @@
 class EventsController < ApplicationController
 
   def show
-    @event = Event.find(params[:id])
+    @event        = Event.find(params[:id])
+    @forecast     = @event.forecasts.first
+    @has_forecast = @event
   end
 end
