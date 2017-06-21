9e23c7a59f1f9ead35d4f52e9278cd303f0d595b
Matthew Saunders <matthew.saunders12@gmail.com>
2017-06-20 21:52:23 -0400

    Add user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root 'dashboard#home'
 
   resources :seasons
   resources :events do
     resources :forecasts
   end
+
+  devise_for :users
 
   # TODO Remove this eventually
   resources :forecasts
