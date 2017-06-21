4c2383558e1592165dd83e169247ec659576e672
Matthew Saunders <matthew.saunders12@gmail.com>
2017-06-20 20:42:37 -0400

    Update forecast spec

--- spec/models/forecast_spec.rb
+++ spec/models/forecast_spec.rb
@@ -9,4 +9,14 @@ RSpec.describe Forecast, type: :model do
   it "is valid with valid attributes" do
     expect(subject).to be_valid
   end
+
+  it "is invalid without a season" do
+    subject.season_id = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is invalid without an event" do
+    subject.event_id = nil
+    expect(subject).to_not be_valid
+  end
 end
